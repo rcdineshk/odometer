ec9f16895fb441a765211f2b82e79726069562e5
krsriram <krupa0308@gmail.com>
2014-08-07 16:12:11 +0530

    Update OdometerProcedural.java

--- OdometerProcedural.java
+++ OdometerProcedural.java
@@ -10,25 +10,36 @@ public class OdometerProcedural {
 	
 	public static String getHighestState(Integer numDigits){
 		String descending = "0123456789";
-		return descending.substring(10 - numDigits , 10);
+		return descending.substring(10 - numDigits);
 		
 	}
 	
-	public static String getNextState(String currentState, Integer numOfDigits){
+	public static String getNthState(String currentState, Integer numOfDigits, Integer n, Integer maximumState){
+		
+		
+		for (int i = 0; i < n; i++){
+			currentState = getNextState(currentState, numOfDigits, maximumState);
+			
+		}
+		
+		return currentState;
+	}
+	
+	public static String getNextState(String currentState, Integer numOfDigits, Integer maximumState){
 		
 		ArrayList<Integer> str2Digits = new ArrayList<Integer>();
 		String nextState = currentState;
 		Integer current = Integer.parseInt(currentState);
 		int nextElement = current;
 	
-		while(nextElement >= current){
-			if(nextElement== Integer.parseInt((getHighestState(numOfDigits)))){
+		while (nextElement >= current){
+			if (nextElement == maximumState){
 				nextElement = Integer.parseInt(initializeOdometer(numOfDigits));
 			}
 			else{
 				nextElement ++;
 			}
-			if(isAscending(Integer.toString(nextElement))){
+			if (isAscending(Integer.toString(nextElement))){
 				String next = Integer.toString(nextElement);
 				if(next.length() == numOfDigits -1)
 				{
@@ -44,20 +55,20 @@ public class OdometerProcedural {
 	public static boolean isAscending(String nextElement){
 		List<Integer> digits = new ArrayList<Integer>();
 		
-		for(int i = 0; i< nextElement.length(); i++){
+		for (int i = 0; i< nextElement.length(); i++){
 			digits.add(Character.getNumericValue(nextElement.charAt(i)));
 		}
 		
 		Collections.sort(digits);
-		for(int i = 0; i<digits.size() -1; i++){
-			if(digits.get(i) == digits.get(i+1)){
+		for (int i = 0; i<digits.size() -1; i++){
+			if (digits.get(i) == digits.get(i+1)){
 				return false;
 			}
 		}
 		String sortedDigits = digits.toString();
 		sortedDigits = sortedDigits.replaceAll("[,\\[\\]\\s]","");
 		
-		if(sortedDigits.equals(nextElement)){
+		if (sortedDigits.equals(nextElement)){
 			return true;
 		}
 		
@@ -68,15 +79,19 @@ public class OdometerProcedural {
 	
 		String currentState;
 		String initialState; 
-		Integer numOfDigits = 5;
-		if(numOfDigits > 10){
+		Integer numOfDigits = 4;
+		int n = 4;
+		if (numOfDigits > 10){
 			System.out.println("Invalid number of Digits provided");
 			System.exit(0);
 		}
 		
 		initialState = initializeOdometer( numOfDigits);
+		
+		Integer highestState = Integer.parseInt(getHighestState(numOfDigits));
 		currentState = initialState;
-		currentState = getNextState(currentState, numOfDigits);
+		currentState = getNextState(currentState, numOfDigits, highestState);
+		currentState = getNthState(currentState, numOfDigits, n , highestState);
 		System.out.println(currentState);
 	}
 
